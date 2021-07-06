e9221e7ffa49238c6444e55edebdcafa403c4e32
unknown <ericadndria2@gmail.com>
2021-07-06 05:32:17 +0200

    Make string function fixed and error detected on __parse

--- expr.py
+++ expr.py
@@ -1,6 +1,7 @@
-#Class file
+# Class file
 from re import findall, search, split
 from typing import Dict
+
 
 class MathExpr:
     __slots__ = {'__repr', '__parsed_tree', '__vars'}
@@ -11,92 +12,46 @@ class MathExpr:
         n = self.__max_depth()
         for _ in range(n):
             self.__parsed_tree = self.__parsed_tree.simplify()
-        self.__repr = self.__parsed_tree.make_string()
+        self.__repr = string
 
     def eval(self, env: Dict[str, float] = {}) -> float:
         for var in env:
-            if not var in self.get_vars():
+            if var not in self.get_vars():
                 raise RuntimeError(f"The function has no variable named: {var}")
         if env != {} and type(list(env.values())[0]) == list:
-            new_env = [dict(zip(list(env.keys()), list(map(lambda x: x[value], list(env.values()))))) for value in range(len(list(env.values())[0]))]
+            new_env = [dict(zip(list(env.keys()), list(map(lambda x: x[value], list(env.values()))))) for value in
+                       range(len(list(env.values())[0]))]
             return [self.__parsed_tree.eval(i) for i in new_env]
         return self.__parsed_tree.eval(env)
 
     def derivate(self, var: str):
-        derivate = MathExpr("0")
-        derivate._MathExpr__parsed_tree = self.__parsed_tree.derivate(var)
-        derivate.__vars = self.__vars
-        n = derivate.__max_depth()
+        exp_derivate = MathExpr("0")
+        exp_derivate._MathExpr__parsed_tree = self.__parsed_tree.derivate(var)
+        exp_derivate.__vars = self.__vars
+        n = exp_derivate.__max_depth()
         for _ in range(n):
-            derivate.__parsed_tree = derivate.__parsed_tree.simplify()
-        derivate._MathExpr__repr = derivate.__parsed_tree.make_string()
-        return derivate
+            exp_derivate.__parsed_tree = exp_derivate.__parsed_tree.simplify()
+        exp_derivate._MathExpr__repr = exp_derivate.__parsed_tree.make_string()
+        return exp_derivate
 
     def get_vars(self):
         return self.__vars
 
-    class __Operator:
+    class __BinOperator:
         __slots__ = {'_left', '_right'}
+
         def __init__(self, left, right):
             self._left = left
             self._right = right
 
         def max_depth(self):
-            return max(self._left.max_depth(), self._right.max_depth())+1
+            return max(self._left.max_depth(), self._right.max_depth()) + 1
 
-    class __Prod(__Operator):
-        def eval(self, env: Dict[str, float]) -> float:
-            return self._left.eval(env) * self._right.eval(env)
+    class __Pow(__BinOperator):
+        @property
+        def PRECEDENCE(self):
+            return 1
 
-        def recHash(self) -> int:
-            return hash(self._left.recHash() * self._right.recHash())
-
-        def derivate(self, var: str):
-            return MathExpr._MathExpr__Sum(MathExpr._MathExpr__Prod(self._left.derivate(var), self._right), 
-                MathExpr._MathExpr__Prod(self._left, self._right.derivate(var)))
-
-        def make_string(self):
-            return self._left.make_string() + '*' + self._right.make_string()
-
-        def simplify(self):
-            if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
-                return MathExpr._MathExpr__Val(self._left.val * self._right.val)
-            elif MathExpr._MathExpr__Val(1) == self._right:
-                return self._left.simplify()
-            elif MathExpr._MathExpr__Val(1) == self._left:
-                return self._right.simplify()
-            elif MathExpr._MathExpr__Val(0) == self._right:
-                return MathExpr._MathExpr__Val(0)
-            elif MathExpr._MathExpr__Val(0) == self._left:
-                return MathExpr._MathExpr__Val(0)
-            else:
-                return MathExpr._MathExpr__Prod(self._left.simplify(), self._right.simplify())
-            
-
-    class __Sum(__Operator):
-        def eval(self, env: Dict[str, float]) -> float:
-            return self._left.eval(env) + self._right.eval(env)
-
-        def recHash(self) -> int:
-            return hash(self._left.recHash() * self._right.recHash())
-
-        def derivate(self, var: str):
-            return MathExpr._MathExpr__Sum(self._left.derivate(var), self._right.derivate(var))
-
-        def make_string(self):
-            return self._left.make_string() + '+' + self._right.make_string()
-
-        def simplify(self):
-            if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
-                return MathExpr._MathExpr__Val(self._left.val + self._right.val)
-            elif MathExpr._MathExpr__Val(0) == self._left:
-                return self._right.simplify()
-            elif MathExpr._MathExpr__Val(0) == self._right:
-                return self._left.simplify()
-            else:
-                return MathExpr._MathExpr__Sum(self._left.simplify(), self._right.simplify())
-
-    class __Pow(__Operator):
         def eval(self, env: Dict[str, float]) -> float:
             return self._left.eval(env) ** self._right.eval(env)
 
@@ -105,10 +60,16 @@ class MathExpr:
 
         def derivate(self, var: str):
             if type(self._right) == MathExpr._MathExpr__Val:
-                return MathExpr._MathExpr__Prod(self._right, MathExpr._MathExpr__Prod(MathExpr._MathExpr__Pow(self._left, MathExpr._MathExpr__Sub(self._right, MathExpr._MathExpr__Val(1.0))), self._left.derivate(var)))
+                return MathExpr._MathExpr__Prod(self._right, MathExpr._MathExpr__Prod(
+                    MathExpr._MathExpr__Pow(self._left,
+                                            MathExpr._MathExpr__Sub(self._right, MathExpr._MathExpr__Val(1.0))),
+                    self._left.derivate(var)))
+            else:
+                raise NotImplemented  # TODO: Implement the c^x type derivate
 
         def make_string(self):
-            return self._left.make_string() + '^' + self._right.make_string()
+            return (self._left.make_string() if self.PRECEDENCE > self._left.PRECEDENCE else "(" + self._left.make_string() + ")") + \
+                '^' + (self._right.make_string() if self.PRECEDENCE > self._right.PRECEDENCE else "(" + self._right.make_string() + ")")
 
         def simplify(self):
             if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
@@ -124,7 +85,44 @@ class MathExpr:
             else:
                 return MathExpr._MathExpr__Pow(self._left.simplify(), self._right.simplify())
 
-    class __Div(__Operator):
+    class __Prod(__BinOperator):
+        @property
+        def PRECEDENCE(self):
+            return 2
+
+        def eval(self, env: Dict[str, float]) -> float:
+            return self._left.eval(env) * self._right.eval(env)
+
+        def recHash(self) -> int:
+            return hash(self._left.recHash() * self._right.recHash())
+
+        def derivate(self, var: str):
+            return MathExpr._MathExpr__Sum(MathExpr._MathExpr__Prod(self._left.derivate(var), self._right),
+                                           MathExpr._MathExpr__Prod(self._left, self._right.derivate(var)))
+
+        def make_string(self):
+            return (self._left.make_string() if self.PRECEDENCE > self._left.PRECEDENCE else "(" + self._left.make_string() + ")") + \
+                '*' + (self._right.make_string() if self.PRECEDENCE > self._right.PRECEDENCE else "(" + self._right.make_string() + ")")
+
+        def simplify(self):
+            if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
+                return MathExpr._MathExpr__Val(self._left.val * self._right.val)
+            elif MathExpr._MathExpr__Val(1) == self._right:
+                return self._left.simplify()
+            elif MathExpr._MathExpr__Val(1) == self._left:
+                return self._right.simplify()
+            elif MathExpr._MathExpr__Val(0) == self._right:
+                return MathExpr._MathExpr__Val(0)
+            elif MathExpr._MathExpr__Val(0) == self._left:
+                return MathExpr._MathExpr__Val(0)
+            else:
+                return MathExpr._MathExpr__Prod(self._left.simplify(), self._right.simplify())
+
+    class __Div(__BinOperator):
+        @property
+        def PRECEDENCE(self):
+            return 2
+
         def eval(self, env: Dict[str, float]) -> float:
             aux = self._right.eval(env)
             if aux != 0:
@@ -136,10 +134,14 @@ class MathExpr:
             return hash(self._left.recHash() // self._right.recHash())
 
         def derivate(self, var: str):
-            return MathExpr._MathExpr__Div(MathExpr._MathExpr__Sub(MathExpr._MathExpr__Prod(self._left.derivate(var), self._right), MathExpr._MathExpr__Prod(self._left, self._right.derivate(var))), MathExpr._MathExpr__Pow(self._right, MathExpr._MathExpr__Val(2.0)))
+            return MathExpr._MathExpr__Div(
+                MathExpr._MathExpr__Sub(MathExpr._MathExpr__Prod(self._left.derivate(var), self._right),
+                                        MathExpr._MathExpr__Prod(self._left, self._right.derivate(var))),
+                MathExpr._MathExpr__Pow(self._right, MathExpr._MathExpr__Val(2.0)))
 
         def make_string(self):
-            return self._left.make_string() + '/' + self._right.make_string()
+            return (self._left.make_string() if self.PRECEDENCE > self._left.PRECEDENCE else "(" + self._left.make_string() + ")") + \
+                '/' + (self._right.make_string() if self.PRECEDENCE > self._right.PRECEDENCE else "(" + self._right.make_string() + ")")
 
         def simplify(self):
             if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
@@ -156,7 +158,39 @@ class MathExpr:
             else:
                 return MathExpr._MathExpr__Div(self._left.simplify(), self._right.simplify())
 
-    class __Sub(__Operator):
+    class __Sum(__BinOperator):
+        @property
+        def PRECEDENCE(self):
+            return 4
+
+        def eval(self, env: Dict[str, float]) -> float:
+            return self._left.eval(env) + self._right.eval(env)
+
+        def recHash(self) -> int:
+            return hash(self._left.recHash() * self._right.recHash())
+
+        def derivate(self, var: str):
+            return MathExpr._MathExpr__Sum(self._left.derivate(var), self._right.derivate(var))
+
+        def make_string(self):
+            return (self._left.make_string() if self.PRECEDENCE > self._left.PRECEDENCE else "(" + self._left.make_string() + ")") + \
+                   '+' + (self._right.make_string() if self.PRECEDENCE > self._right.PRECEDENCE else "(" + self._right.make_string() + ")")
+
+        def simplify(self):
+            if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
+                return MathExpr._MathExpr__Val(self._left.val + self._right.val)
+            elif MathExpr._MathExpr__Val(0) == self._left:
+                return self._right.simplify()
+            elif MathExpr._MathExpr__Val(0) == self._right:
+                return self._left.simplify()
+            else:
+                return MathExpr._MathExpr__Sum(self._left.simplify(), self._right.simplify())
+
+    class __Sub(__BinOperator):
+        @property
+        def PRECEDENCE(self):
+            return 3
+
         def eval(self, env: Dict[str, float]) -> float:
             return self._left.eval(env) - self._right.eval(env)
 
@@ -167,7 +201,8 @@ class MathExpr:
             return MathExpr._MathExpr__Sub(self._left.derivate(var), self._right.derivate(var))
 
         def make_string(self):
-            return self._left.make_string() + '-' + self._right.make_string()
+            return (self._left.make_string() if self.PRECEDENCE > self._left.PRECEDENCE else "(" + self._left.make_string() + ")") + \
+                '-' + (self._right.make_string() if self.PRECEDENCE > self._right.PRECEDENCE else "(" + self._right.make_string() + ")")
 
         def simplify(self):
             if MathExpr._MathExpr__Val == type(self._right) and MathExpr._MathExpr__Val == type(self._left):
@@ -180,6 +215,10 @@ class MathExpr:
                 return MathExpr._MathExpr__Sub(self._left.simplify(), self._right.simplify())
 
     class __Val:
+        @property
+        def PRECEDENCE(self):
+            return 0
+
         def __init__(self, val):
             self.val = val
 
@@ -207,9 +246,13 @@ class MathExpr:
             return False
 
     class __Var:
+        @property
+        def PRECEDENCE(self):
+            return 1
+
         def __init__(self, name):
             self.name = name
-        
+
         def eval(self, env: Dict[str, float]) -> float:
             return env[self.name]
 
@@ -243,31 +286,41 @@ class MathExpr:
 
     def __mul__(self, other):
         ret = MathExpr("0")
-        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(self.__parsed_tree) == self.__Val else f"({self.__repr})") + '*' + (str(other) if type(other.__parsed_tree) == self.__Var or type(other.__parsed_tree) == self.__Val else f"({str(other)})")
+        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(
+            self.__parsed_tree) == self.__Val else f"({self.__repr})") + '*' + (
+                         str(other) if type(other.__parsed_tree) == self.__Var or type(
+                             other.__parsed_tree) == self.__Val else f"({str(other)})")
         ret.__parsed_tree = self.__Prod(self.__parsed_tree, other.__parsed_tree)
         return ret
 
     def __truediv__(self, other):
         ret = MathExpr("0")
-        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(self.__parsed_tree) == self.__Val else f"({self.__repr})") + '/' + (str(other) if type(other.__parsed_tree) == self.__Var or type(other.__parsed_tree) == self.__Val else f"({str(other)})")
+        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(
+            self.__parsed_tree) == self.__Val else f"({self.__repr})") + '/' + (
+                         str(other) if type(other.__parsed_tree) == self.__Var or type(
+                             other.__parsed_tree) == self.__Val else f"({str(other)})")
         ret.__parsed_tree = self.__Div(self.__parsed_tree, other.__parsed_tree)
         return ret
 
     def __sub__(self, other):
         ret = MathExpr("0")
-        ret.__repr = self.__repr  + '-' + (str(other) if type(other.__parsed_tree) == self.__Var or type(other.__parsed_tree) == self.__Val else f"({str(other)})")
+        ret.__repr = self.__repr + '-' + (str(other) if type(other.__parsed_tree) == self.__Var or type(
+            other.__parsed_tree) == self.__Val else f"({str(other)})")
         ret.__parsed_tree = self.__Sub(self.__parsed_tree, other.__parsed_tree)
         return ret
 
     def __pow__(self, other):
         ret = MathExpr("0")
-        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(self.__parsed_tree) == self.__Val else f"({self.__repr})") + '^' + (str(other) if type(other.__parsed_tree) == self.__Var or type(other.__parsed_tree) == self.__Val else f"({str(other)})")
+        ret.__repr = (self.__repr if type(self.__parsed_tree) == self.__Var or type(
+            self.__parsed_tree) == self.__Val else f"({self.__repr})") + '^' + (
+                         str(other) if type(other.__parsed_tree) == self.__Var or type(
+                             other.__parsed_tree) == self.__Val else f"({str(other)})")
         ret.__parsed_tree = self.__Pow(self.__parsed_tree, other.__parsed_tree)
         return ret
 
     def __str__(self):
         return self.__repr
-    
+
     def __hash(self):
         return self.__parsed_tree.recHash()
 
@@ -289,8 +342,8 @@ class MathExpr:
                     count_par += 1
                 elif string[i] == ')':
                     if count_par == 0:
-                        parenthesis.append(string[first_pos+1:i])
-                        string = string.replace('('+parenthesis[-1]+')', '#'+str(len(parenthesis)-1)+'#')
+                        parenthesis.append(string[first_pos + 1:i])
+                        string = string.replace('(' + parenthesis[-1] + ')', '#' + str(len(parenthesis) - 1) + '#')
                         break
                     else:
                         count_par -= 1
@@ -309,7 +362,7 @@ class MathExpr:
             if divmul[0] == "*":
                 return self.__Prod(self.__parse_rec(new[0], parenthesis), self.__parse_rec(new[1], parenthesis))
             if divmul[0] == "/":
-                return self.__Div(self.__parse_rec(new[0], parenthesis), self. __parse_rec(new[1], parenthesis))
+                return self.__Div(self.__parse_rec(new[0], parenthesis), self.__parse_rec(new[1], parenthesis))
         elif findall(r"\^", string):
             new = string.split("^", 1)
             return self.__Pow(self.__parse_rec(new[0], parenthesis), self.__parse_rec(new[1], parenthesis))
@@ -337,8 +390,9 @@ class MathExpr:
         elif findall(r"\+{2,}|\-{2,}|\*{2,}|\/{2,}|\^{2,}", string):
             raise RuntimeError("Illegal operation")
         for var in split(r"[\+\*\-\/\^]", string):
+            var = "".join(list(filter(lambda x: x != "(" and x != ")", var)))
             try:
-                int(var)
-            except:
+                float(var)
+            except ValueError:
                 if '0' < var[0] < '9':
                     raise RuntimeError("Variable name cannot begin with a number")
--- test_expr.py
+++ test_expr.py
@@ -1,4 +1,4 @@
-#Main file
+# Main file
 from expr import *
 
 
@@ -9,22 +9,31 @@ e4 = MathExpr("y*2")
 e5 = MathExpr("4*y^2")
 e6 = MathExpr("x*2+x/2-x^2")
 
+
 def test_eval():
     assert e.eval({'x': 0}) == float("inf")
     assert e.eval({'x': 5}) == 64.8
     assert e2.eval({"x1x1x": 2}) == 4.0
 
+
 def test_get_vars():
     assert e.get_vars() == {'x'}
     assert e2.get_vars() == {'x1x1x'}
+
 
 def test_equalty():
     assert e3 == e4
     assert e3 != e5
 
+
 def test_multiple_eval():
     assert e3.eval({'y': [10, 20, 30]}) == [20, 40, 60]
 
+
 def test_derivate():
-    assert f"{e.derivate('x').eval({'x': 5}):.2f}" == "-5.04" #Problem with aproximation
+    assert f"{e.derivate('x').eval({'x': 5}):.2f}" == "-5.04"  # Problem with aproximation
     assert e6.derivate('x').eval({'x': 5}) == -7.5
+
+
+def test_make_string():
+    assert f"{MathExpr(str(e.derivate('x'))).eval({'x': 5}):.2f}" == "-5.04"  # TODO: Fix "__check_string" and "__parse"
